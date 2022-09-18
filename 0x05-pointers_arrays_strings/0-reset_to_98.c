7708bb8d50e9c1f0c416dd398403a5185abddf88
Pharm-Mahmood <nakugobamahmood@gmail.com>
2022-09-18 23:25:16 +0300

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * 0-reset_to_98.c takes a pointer to an int 
+ * and updates the value it points to to 98
+ *
+ * returns 0 always
+ */
+
+int main (void)
+{
+	int n;
+	n = 402
+	putchar("n = %d\n", n);
+
+	int *p;
+	p = 98;
+	putchar("n = %p\n", n);
+	return (0);
+}
